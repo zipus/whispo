2cc3bc0b6e93b6f9d908247fddb241e419b82f48
EGOIST <hi@egoist.dev>
2024-10-22 14:40:26 +0800

    fix: update tray icon for windows

--- src/main/tray.ts
+++ src/main/tray.ts
@@ -8,7 +8,7 @@ import {
 } from "./window"
 import { state } from "./state"
 
-const defaultIcon = path.join(__dirname, "../../resources/trayIconTemplate.png")
+const defaultIcon = path.join(__dirname, `../../resources/${process.env.IS_MAC ? 'trayIconTemplate.png' : 'trayIcon.ico'}`)
 const stopIcon = path.join(
   __dirname,
   "../../resources/stopTrayIconTemplate.png",
